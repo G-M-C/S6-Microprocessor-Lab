24f110c1fb668954429c865a1c6d9e72fc456f36
George M Cherian <42828441+G-M-C@users.noreply.github.com>
2021-04-12 23:09:03 +0530

    Single Digit Subtraction

---
+++ EXP-5/sub(s).asm
@@ -0,0 +1,50 @@
+ASSUME DS:DATA,CS:CODE
+
+DATA SEGMENT
+MSG1 DB 0AH,0DH,"ENTER FIRST NUMBER:$"
+MSG2 DB 0AH,0DH,"ENTER SECOND NUMBER:$"
+MSG3 DB 0AH,0DH,"THE ANSWER IS:$"
+DATA ENDS
+
+CODE SEGMENT
+START:
+      MOV AX,DATA
+      MOV DS,AX
+
+      LEA DX,MSG1
+      MOV AH,09H
+      INT 21H
+      MOV AH,01H
+      INT 21H
+      MOV CL,AL
+
+
+      LEA DX,MSG2
+      MOV AH,09H
+      INT 21H
+      MOV AH,01H
+      INT 21H
+      MOV BL,AL
+
+
+      LEA DX,MSG3
+      MOV AH,09H
+      INT 21H
+      MOV AH,00H
+      SUB CL,BL
+      MOV AL,CL
+      AAS
+      ADD AL,30H
+      MOV BL,AL
+
+
+
+      MOV DL,BL
+      MOV AH,02H
+      INT 21H
+
+      MOV AH,4CH
+      INT 21H
+
+CODE ENDS
+END START
